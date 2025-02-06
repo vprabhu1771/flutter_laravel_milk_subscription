a37a411cc58f48fee1a04c8c39cfb059651cd5c4
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-06 12:03:16 +0530

    dark light theme fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_laravel_milk_subscription/screens/SplashScreen.dart';
+import 'package:flutter_laravel_milk_subscription/services/UiProvider.dart';
+import 'package:flutter_laravel_milk_subscription/screens/SettingScreen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
 
@@ -14,9 +17,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    return ChangeNotifierProvider(
+      create: (BuildContext context)=>UiProvider()..init(),
+      child: Consumer<UiProvider>(
+        builder: (context, UiProvider notifier, child) {
+          return MaterialApp(
+
+            debugShowCheckedModeBanner: false,
+
+            title: 'Dark Theme',
+            //By default theme setting, you can also set system
+            // when your mobile theme is dark the app also become dark
+
+            themeMode: notifier.isDark ? ThemeMode.dark : ThemeMode.light,
+
+            //Our custom theme applied
+            darkTheme: notifier.isDark ? notifier.darkTheme : notifier.lightTheme,
+
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+
+            home: SplashScreen(),
+
+          );
+        },
+      ),
     );
   }
 }
