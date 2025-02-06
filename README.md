0d6b983aa31d4eef0963a1c17d96888d6ae464a8
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-06 11:44:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_laravel_milk_subscription
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_laravel_milk_subscription/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_laravel_milk_subscription
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
