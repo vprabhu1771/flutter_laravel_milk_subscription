397343855690b4e9c8ba7ae318dfd455d7e4cecf
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-09 13:55:47 +0530

    LoginScreen after login go to HomePage

--- lib/screens/auth/LoginScreen.dart
+++ lib/screens/auth/LoginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_laravel_milk_subscription/screens/HomePage.dart';
 import 'package:flutter_laravel_milk_subscription/screens/HomeScreen.dart';
 import 'package:flutter_laravel_milk_subscription/screens/auth/RegisterScreen.dart';
 
@@ -135,7 +136,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
                     Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => HomeScreen(title: 'Home',))
+                        MaterialPageRoute(builder: (context) => HomePage())
                     );
 
                   },
