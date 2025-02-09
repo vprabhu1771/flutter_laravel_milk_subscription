08aa966a8f2ac212060c2d3ff9ea542759327552
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-09 13:29:32 +0530

    Bypass SSL Shared Hosting Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_laravel_milk_subscription/screens/SplashScreen.dart';
-import 'package:flutter_laravel_milk_subscription/screens/auth/LoginScreen.dart';
-import 'package:flutter_laravel_milk_subscription/screens/auth/RegisterScreen.dart';
 import 'package:flutter_laravel_milk_subscription/services/UiProvider.dart';
-import 'package:flutter_laravel_milk_subscription/screens/SettingScreen.dart';
 import 'package:provider/provider.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+
+  HttpOverrides.global = MyHttpOverrides();
 
   WidgetsFlutterBinding.ensureInitialized();
 
--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -1,7 +1,10 @@
 class Constants  {
 
-  static const String BASE_URL = "http://192.168.1.122:8000/api";
+  // static const String BASE_URL = "http://192.168.1.122:8000/api";
+  static const String BASE_URL = "https://bughunt.in/api";
 
   static const String PRODUCT_ROUTE = "/products";
+
+  static const String SUBSCRIPTION_PLANS_ROUTE = "/subscription-plans";
 
 }
